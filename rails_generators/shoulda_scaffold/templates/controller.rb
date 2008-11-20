86c8456863773bb86b6708f637aa2b2496344f6c
Stefan Penner <stefan@stefan-penners-macbook-pro.local>
2008-11-19 22:05:45 -0600

    testing

--- rails_generators/shoulda_scaffold/templates/controller.rb
+++ rails_generators/shoulda_scaffold/templates/controller.rb
@@ -1,29 +1,14 @@
 class <%= controller_class_name %>Controller < ApplicationController
   def index
     @<%= table_name %> = <%= class_name %>.find(:all)
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @<%= table_name %> }
-    end
   end
 
   def show
     @<%= file_name %> = <%= class_name %>.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @<%= file_name %> }
-    end
   end
 
   def new
     @<%= file_name %> = <%= class_name %>.new
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @<%= file_name %> }
-    end
   end
 
   def edit
@@ -33,40 +18,29 @@ class <%= controller_class_name %>Controller < ApplicationController
   def create
     @<%= file_name %> = <%= class_name %>.new(params[:<%= file_name %>])
 
-    respond_to do |format|
-      if @<%= file_name %>.save
-        flash[:notice] = '<%= class_name %> was successfully created.'
-        format.html { redirect_to(@<%= file_name %>) }
-        format.xml  { render :xml => @<%= file_name %>, :status => :created, :location => @<%= file_name %> }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @<%= file_name %>.errors, :status => :unprocessable_entity }
-      end
+    if @<%= file_name %>.save
+      flash[:success] = '<%= class_name %> was successfully created.'
+      redirect_to(@<%= file_name %>)
+    else
+      render :action => "new" 
     end
   end
 
   def update
     @<%= file_name %> = <%= class_name %>.find(params[:id])
 
-    respond_to do |format|
-      if @<%= file_name %>.update_attributes(params[:<%= file_name %>])
-        flash[:notice] = '<%= class_name %> was successfully updated.'
-        format.html { redirect_to(@<%= file_name %>) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @<%= file_name %>.errors, :status => :unprocessable_entity }
-      end
+    if @<%= file_name %>.update_attributes(params[:<%= file_name %>])
+      flash[:success] = '<%= class_name %> was successfully updated.'
+      redirect_to(@<%= file_name %>) 
+    else
+      render :action => "edit" 
     end
   end
 
   def destroy
     @<%= file_name %> = <%= class_name %>.find(params[:id])
     @<%= file_name %>.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(<%= table_name %>_url) }
-      format.xml  { head :ok }
-    end
+    
+    redirect_to(<%= table_name %>_url)
   end
 end
